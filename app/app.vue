77b1635434bac88edb507e8c9098685705103b39
Eyob Muktar <45830178+eyob-muktar@users.noreply.github.com>
2025-11-10 23:42:46 +0300

    setup: tailwind / daisyui (#49)

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="font-black underline">
+      Hello World
+    </h1>
+    <button class="btn btn-secondary">
+      A DaisyUI Button
+      <Icon name="tabler:map" />
+    </button>
   </div>
 </template>
