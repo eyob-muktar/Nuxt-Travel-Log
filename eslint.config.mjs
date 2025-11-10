bf1684528d7e08ba547e54756cfd000f3e7503b4
Eyob Muktar <45830178+eyob-muktar@users.noreply.github.com>
2025-11-10 21:09:54 +0300

    setup: eslint / lint-staged (#48)
    * setup: eslint / lint-staged
    
    * setup: add lint workflow
    
    * fix: pnpm version
    
    * fix: pnpm version
    
    * fix: Update Node.js version in lint workflow

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import antfu from '@antfu/eslint-config';
+
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt((antfu({
+  type: 'app',
+  vue: true,
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+    semi: true,
+  },
+  ignores: ['.pnpm-store/**'],
+}, {
+  rules: {
+    'perfectionist/sort-imports': ['error', {
+      tsconfigRootDir: '.',
+    }],
+    'unicorn/filename-case': ['error', {
+      case: 'kebabCase',
+      ignore: [
+        'nuxt.config.ts',
+        'eslint.config.mjs',
+        'README.md',
+      ],
+    }],
+  },
+})),
+);
