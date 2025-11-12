1dde417b13b6d1c58be9658c8f4b805f374254d5
eyob-muktar <eyobmuktar4@gmail.com>
2025-11-12 18:30:18 +0300

    feat: add profile and signup buttons

--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -1,25 +1,27 @@
 import { authClient } from '../lib/auth-client';
 
-export const useAuthStore = defineStore('authStore', () => {
-  const loading = ref(false);
+export const useAuthStore = defineStore('useAuthStore', () => {
+  const session = authClient.useSession();
+  const user = computed(() => session.value.data?.user);
+  const loading = computed(() => session.value.isPending || session.value.isRefetching);
 
   async function signIn() {
-    loading.value = true;
     await authClient.signIn.social({
       provider: 'github',
       callbackURL: '/dashboard',
       errorCallbackURL: '/error',
     });
-    loading.value = false;
   }
 
   async function signOut() {
     await authClient.signOut();
+    navigateTo('/');
   }
 
   return {
     loading,
     signIn,
     signOut,
+    user,
   };
 });
